e3b237c36ef3aceb5e94bf7e9970590dbb438037
tony albanese <tony.t.albanese@gmail.com>
2023-10-03 14:07:41 -0400

    added parameter joining, filtered out problem muni

--- preprocessing/download_gee.py
+++ preprocessing/download_gee.py
@@ -8,15 +8,41 @@ import click
 #import geopandas as gpd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import os
+from pebble import ProcessPool
+import polars as pl
+import glob
 
 @click.command
 @click.option('--config-file', type=str, default='preprocessing/gee_config.toml')
-def main(config_file): 
+@click.option('--clean', type=bool, default=True)
+def main(config_file, clean): 
     with open(config_file, 'rb') as f:
         config = tomllib.load(f)
 
-    populate_assets(config)
-    download_all(config)
+    #populate_assets(config)
+    #download_all(config)
+    join_all(config)
+    #if clean:
+    #    cleanup(config)
+
+def join_all(config):
+    all_parquets = glob.glob(os.path.join(config['base']['save_dir'], '*.parquet'))
+    check_base_exists = glob.glob(os.path.join(config['base']['save_dir'], 'all_parameters_*'))
+    if len(check_base_exists)>0:
+        all_parquets = list(set(all_parquets)-set(check_base_exists))
+    base_df = pl.read_parquet(all_parquets[0])
+    if len(all_parquets)>1:
+        for f in all_parquets[1:]:
+            base_df = base_df.join(pl.read_parquet(f), on=['muni_id', 'start_date', 'end_date'])
+    base_df.write_parquet(os.path.join(config['base']['save_dir'],f'all_parameters_{config["start_date"]}_{config["end_date"]}.parquet'))
+    return True
+
+
+def cleanup(config):
+    all_csvs = glob.glob(os.path.join(config['base']['save_dir'], '*.csv'))
+    for csv in all_csvs:
+        os.remove(csv)
 
 def populate_assets(config):
     #assets = dict()
@@ -35,14 +61,14 @@ def populate_assets(config):
 def generate_requests(config, dataset):
     start_date, end_date = datetime.fromisoformat(config['base']['start_date']), datetime.fromisoformat(config['base']['end_date'])
     requests = list()
-
     while start_date < end_date:
         requests.append({
-            'start_date': start_date,
+            'start_date': start_date.isoformat(),
             'num_units': config['base']['agg_chunks'], #if config['base']['agg_unit']+start_date < end_date else end_date-start_date
             'collection': dataset['collection'],
             'parameter': dataset['parameter'],
-            'time_unit': config['base']['agg_unit']
+            'time_unit': config['base']['agg_unit'],
+            'save_dir': config['base']['save_dir']
         })
         start_date = start_date + relativedelta(**{config['base']['agg_unit']:config['base']['agg_chunks']})
         
@@ -54,9 +80,39 @@ def download_all(config):
     for dataset in config['datasets']:
         #Need to split up time otherwise we reach GEE limits
         requests = generate_requests(config, dataset)
-        pass
-        #export_over_time(config['base']['start_month_1'], config['base']['num_months_1'], dataset['collection'], dataset['parameter'])
-        #export_over_time(config['base']['start_month_2'], config['base']['num_months_2'], dataset['collection'], dataset['parameter'])
+        fulfill_requests(requests, config)
+        merge_downloads(config, dataset)
+    return True
+
+
+def merge_downloads(config, dataset):
+    (
+        pl.read_csv(os.path.join(config["base"]["save_dir"], f'{dataset["parameter"]}_*.csv'), try_parse_dates=True)
+        .rename({'median': dataset["parameter"],
+                 'CD_MUN': 'muni_id'})
+        .sort(['muni_id', 'start_date'])
+        .fill_null(-999)
+        #Filter out munis that are just lakes
+        .filter(pl.col('muni_id')!= 430000)
+        .write_parquet(os.path.join(config["base"]["save_dir"], f'{dataset["parameter"]}_{config["base"]["start_date"]}_{config["base"]["end_date"]}.parquet'))
+    )
+
+
+def fulfill_requests(requests, config):
+    with ProcessPool(max_workers=config['base']['max_workers']) as pool:
+        future = pool.map(export_over_time, requests, timeout=600)
+        iterator = future.result()
+
+        while True:
+            try:
+                result = next(iterator)
+            except StopIteration:
+                break
+            except TimeoutError as error:
+                print(error)
+            except Exception as error:
+                print(error)
+    return True
 
 #Helper fns
 #We have municipios split into two chunks since there are over 5000 munis which is over the GEE feature collection limit
@@ -92,11 +148,14 @@ def agg_to_munis(img):
     return img_stats_1.merge(img_stats_2)
 
 
-def export_over_time(start_date, num_units, collection, parameter, time_unit):
+#def export_over_time(start_date, num_units, collection, parameter, time_unit, save_dir):
+def export_over_time(args_dict):
     #relativedelta uses plural units, GEE uses singular
-    time_unit = time_unit[:-1]
-    col = ee.ImageCollection(collection).select(parameter)
-    base_date = ee.Date(start_date)
+    time_unit = args_dict["time_unit"][:-1]
+    print(args_dict)
+    col = ee.ImageCollection(args_dict["collection"]).select(args_dict["parameter"])
+    print(col.propertyNames())
+    base_date = ee.Date(args_dict["start_date"])
 
     def month_mapper(n):
         return agg_to_munis(
@@ -117,14 +176,16 @@ def export_over_time(start_date, num_units, collection, parameter, time_unit):
         )
 
     month_ranges = ee.FeatureCollection(
-        ee.List.sequence(0, num_units)
+        #Sequence is inclusive []
+        ee.List.sequence(0, args_dict["num_units"]-1)
         .map(month_mapper)
     ).flatten()
 
     #print(month_ranges)
     geemap.common.ee_export_vector(month_ranges, 
-                                   'test.csv',
+                                   os.path.join(args_dict["save_dir"], f'{args_dict["parameter"]}_{args_dict["start_date"]}.csv'),
                                    selectors = ['CD_MUN', 'median', 'start_date', 'end_date'])
+    return True
 
 if __name__ == '__main__':
     #global nonsense to accomodate GEE
