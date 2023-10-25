05135e61578f3f3a609e5fc75d801bab3e2d3dbe
tony albanese <tony.t.albanese@gmail.com>
2023-10-25 19:29:23 -0400

    working on analysis functionality

---
+++ post_analysis/analysis_app.py
@@ -0,0 +1,92 @@
+import streamlit as st
+from metaflow import Flow
+import geopandas as gpd
+import pandas as pd
+import pydeck as pdk
+
+#Goal: 1 data frame of all model results
+#       Select on that, we get a dataframe of specific results for that model
+#       We also get: metrics map, residuals, various other plots
+#       Side by side??
+
+st.title('Analysis Test')
+
+@st.cache_data
+def load_results():
+    return pd.read_csv('./data/temp/prediction_metrics.csv').astype({'item_id': 'string'})
+
+@st.cache_data
+def load_gdf() -> gpd.GeoDataFrame:
+    munis =  gpd.read_file('./data/brazil/munis/munis_simple.shp').astype({'CD_MUN': 'string'}).to_crs('EPSG:4326')
+    munis['CD_MUN'] = munis['CD_MUN'].str.slice(stop=-1)
+    return munis.rename(columns={'CD_MUN': 'item_id'})
+
+@st.cache_resource
+def join_items(gdf, results):
+    return gdf.merge(results, on='item_id')
+
+
+results = load_results()
+gdf = load_gdf()
+
+st.write(results)
+#st.write(gdf)
+
+
+joined = join_items(gdf, results)
+
+#st.write(joined)
+
+def update_deck():
+    layer.get_elevation = option
+
+    deck.update()
+    pass
+
+option = st.selectbox(
+    'Metric',
+    ('MSE', 'abs_error'),
+    on_change=update_deck)
+
+st.write(option)
+
+layer = pdk.Layer(
+           'GeoJsonLayer',
+           data=joined,
+           opacity=0.8,
+    stroked=True,
+    filled=True,
+           #get_polygon = joined.geometry,
+           #stroked=False,
+    # processes the data as a flat longitude-latitude pair
+    #get_polygon='-',
+    get_fill_color='[180,0,180, abs_error+20]',
+        #    radius=200,
+        #    elevation_scale=4,
+        #    elevation_range=[0, 1000],
+           pickable=True,
+           #get_elevation = 'abs_target_sum',
+           get_elevation = 'abs_error',
+           elevation_scale = 500,
+           extruded=True,
+        #    extruded=True,
+        )
+deck = pdk.Deck(
+    map_style=None,
+    layers=[layer],
+    tooltip={
+   "html": "<b>{NM_MUN}</b> <br><b>Abs Error:</b> {abs_error} <br> <b>Actual Case Rate:</b> {abs_target_sum}",
+   "style": {
+        "backgroundColor": "steelblue",
+        "color": "white"
+   }
+},
+    initial_view_state=pdk.ViewState(latitude=-14,
+        longitude=-50,
+        zoom=4,
+        pitch=30)
+)
+
+st.pydeck_chart(deck)
+
+#st.write(layer.get_elevation)
---
+++ post_analysis/populate_db.py
@@ -0,0 +1,130 @@
+import duckdb
+from metaflow import Metaflow
+import pandas as pd
+import numpy as np
+
+class DBBuilder():
+    def __init__(self, db_loc = ':memory:'):
+        self.handler_dict = {
+            'config': self.handle_config,
+            'forecasts': self.handle_predictions
+        }
+        self.con = duckdb.connect(database=db_loc)
+        if db_loc == ':memory:':
+            self.init_tables()
+        self.find_all_data()
+        
+        self.all_data = {
+            '':''
+        }
+
+        
+    
+        
+    def init_tables(self):
+        self.init_model_runs_table()
+        self.init_predictions_table()
+        pass
+
+    def init_model_runs_table(self):
+        self.con.sql("""CREATE TABLE model_runs
+                     (
+                        modelId UBIGINT PRIMARY KEY,
+                        modelType VARCHAR,
+                        trainLimit DATE,
+                        validLimit DATE,
+                        testLimit DATE,
+                        context_length UTINYINT,
+                        batchSize USMALLINT,
+                        predictionLength UTINYINT,
+                        trainEpochs UTINYINT,
+                        frequency VARCHAR,
+                        staticFeatures VARCHAR[],
+                        dynamicFeatures VARCHAR[],
+                        modelArgs VARCHAR,
+                     )
+                     """)
+        
+    def init_predictions_table(self):
+        self.con.sql("""CREATE TABLE predictions
+                     (
+                        model_id BIGINT,
+                        muni_id VARCHAR,
+                        start_date DATETIME,
+                        predict_date DATETIME,
+                        step UTINYINT,
+                        time_unit VARCHAR,
+                        mean DOUBLE,
+                        std DOUBLE
+                     )
+                    """)
+        
+
+    def find_all_data(self):
+        #Find all metaflow data artifacts from model runs
+        flows = Metaflow().flows
+        for flow in flows:
+            for run in flow:
+                if run.data != None:
+                    for entry in run.data._artifacts.items():
+                        self.handle_data_artifact(entry)
+    
+    def handle_data_artifact(self, entry):
+        key, artifact = entry
+        run_id = artifact._object['run_number']
+        if key in self.handler_dict:
+            self.handler_dict[key](artifact, run_id)
+
+    def handle_config(self, artifact, run_id):
+        cfg = artifact.data
+
+    def handle_item_metrics(self, artifact, run_id):
+        pass
+    def handle_agg_metrics(self, artifact, run_id):
+        pass
+    def handle_actual_values(self):
+        pass
+
+    def handle_predictions(self, artifact, run_id):
+        for forecast in artifact.data:
+            self.handle_prediction(forecast, run_id)
+
+    def handle_prediction(self, sample_forecast, run_id):
+        dates = sample_forecast._index.start_time
+        period = sample_forecast._index.freqstr
+        muni_id = sample_forecast.item_id
+        means = sample_forecast.samples.mean(axis=0)
+        stds = sample_forecast.samples.std(axis=0)
+        start_date = dates[0]
+        num_entries = len(dates)
+
+        df = pd.DataFrame.from_dict(
+            {
+                'model_id': pd.Series([int(run_id)]*num_entries, dtype=np.int64),
+                'muni_id' : pd.Series([muni_id]*num_entries, dtype='string'),
+                'start_date': pd.Series([start_date]*num_entries),
+                'predict_date': dates,
+                'step': pd.Series(list(range(num_entries)), dtype=np.int8),
+                'time_unit' : pd.Series([period]*num_entries, dtype='string'),
+                'mean' : pd.Series(means, dtype=np.float32),
+                'std' : pd.Series(stds, dtype=np.float32),          
+             })
+        #self.con.append('predictions', df)
+        self.con.execute('INSERT INTO predictions SELECT * from df')
+
+
+
+        
+
+
+
+
+if __name__ == '__main__':
+    test = DBBuilder()
+    print(test.con.sql("SELECT * FROM predictions"))
+
+    #import toml
+
+
+    
+
